e2d7c1d003208a0cbdf2cb872f36f111f7ab7b63
thiago-shibata <thiago.shibatta@gmail.com>
2024-11-20 21:45:52 -0300

    connect api with frontend && creating inicialization Flask SocketIO

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payments_pix():
@@ -25,9 +27,9 @@ def create_payments_pix():
 
   new_payment = Payment(value=data['value'], expiration_date=expiration_date)
   
+  # criando o objeto pix que fará comunicação com o banco para gerar o código pix e o qr_code
   pix_obj = Pix()
   data_payment_pix = pix_obj.create_payment()
-
   new_payment.bank_payment_id = data_payment_pix['bank_payment_id']
   new_payment.qr_code = data_payment_pix['qr_code_path']
   
@@ -37,6 +39,7 @@ def create_payments_pix():
   return jsonify({"message":"The payment has been created", 
                   "payment": new_payment.to_dict()})
 
+# Rota que retorna uma imagem armazenada na pasta static/img
 @app.route('/payments/pix/qr_code/<file_name>', methods=['GET'])
 def get_image(file_name):
   return send_file(f"static/img/{file_name}.png", mimetype='image/png')
@@ -48,7 +51,13 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-  return 'pagamento pix'
+  payment = Payment.query.get(payment_id)
+  return render_template('payment.html', 
+                         payment_id=payment.id, 
+                         value=payment.value,
+                         host='http://127.0.0.1:5000',
+                         qr_code=payment.qr_code
+                         )
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  socketio.run(app,debug=True)
